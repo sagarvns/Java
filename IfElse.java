85d723683e9980b863ecbc5293d28e7317b2aae3
Sagar Chauhan <jrsagaroo7@gmail.com>
2023-06-16 20:42:07 +0530

    change

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+public class IfElse {
+    public static void main(String args[])
+    {
+        int a=23;
+        int b= 34;
+        if (a > b)
+        {
+            System.out.println(" Greater number A");
+        }
+        else
+        {
+             System.out.println(" Greater number B ");
+        }
+    }
+}
